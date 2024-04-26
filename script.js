b8162cd5d3800d07f682db9fd723e3133c035fb0
Aliaksei-Zaviarzhenets <alex.zaviarzhenets12@gmail.com>
2024-04-26 20:44:14 +0200

    serve files

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const { readFileSync } = require('fs');
+const { Server } = require('http');
+const server = new Server();
+const port = 3000;
+
+server.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
+
+
+
+
+server.addListener('request', (req, res) => {
+  if (req.url === '/') {
+    res.end(readFileSync('index.html'));
+  } else if (req.url === '/favicon.ico') {
+    res.end(readFileSync('favicon.ico'));
+  } else if (req.url === '/index.html') {
+    res.end(readFileSync('index.html'));
+  } else if (req.url === '/style.css') {
+    res.end(readFileSync('style.css'));
+  } else if (req.url === '/script.js') {
+    res.end(readFileSync('script.js'));
+  } else {
+    res.end('404');
+  }
+
+
+
+
+
+})
